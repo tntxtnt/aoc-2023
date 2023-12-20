4ece2d0ad702c225c152f2b58f7896b841c613e4
Tri Tran <lepnit205@gmail.com>
2023-12-20 11:49:25 -0600

    Day 19 20

---
+++ day19/src/day19.cpp
@@ -0,0 +1,227 @@
+#include <fmt/format.h>
+#include <fmt/color.h>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+#include <ranges>
+
+static constexpr std::string_view kInputFilename = "day19.txt";
+
+struct PartRatings {
+    int x{};
+    int m{};
+    int a{};
+    int s{};
+    explicit PartRatings(std::string_view sv) {
+        x = std::stoi(sv.substr(sv.find("x=") + 2).data());
+        m = std::stoi(sv.substr(sv.find("m=") + 2).data());
+        a = std::stoi(sv.substr(sv.find("a=") + 2).data());
+        s = std::stoi(sv.substr(sv.find("s=") + 2).data());
+    }
+    int get(std::string_view label) const { return label == "x" ? x : label == "m" ? m : label == "a" ? a : s; }
+};
+struct PartRatingCompare {
+    std::string label;
+    bool lt;
+    int value;
+    std::string next;
+    explicit PartRatingCompare(std::string_view sv) {
+        label = sv.substr(0, 1);
+        lt = sv[1] == '<';
+        value = std::stoi(sv.substr(2, sv.find(":") - 2).data());
+        next = sv.substr(sv.find(":") + 1);
+    }
+    bool operator()(const PartRatings& part) const { return lt ? part.get(label) < value : part.get(label) > value; }
+};
+struct Workflow {
+    std::string name;
+    std::vector<PartRatingCompare> steps;
+    std::string last;
+    Workflow() = default;
+    explicit Workflow(std::string_view sv) {
+        name = sv.substr(0, sv.find("{"));
+        sv = sv.substr(sv.find("{") + 1);
+        while (sv.find(",") != sv.npos) {
+            std::string_view s = sv.substr(0, sv.find(","));
+            steps.emplace_back(PartRatingCompare{s});
+            sv = sv.substr(sv.find(",") + 1);
+        }
+        last = sv.substr(0, sv.size() - 1);
+    }
+    std::string operator()(const PartRatings& part) const {
+        for (auto& step : steps)
+            if (step(part)) return step.next;
+        return last;
+    }
+};
+using Input = std::pair<std::unordered_map<std::string, Workflow>, std::vector<PartRatings>>;
+
+Input parseInput(std::istream& in) {
+    Input res;
+    for (std::string line; std::getline(in, line);) {
+        Workflow workflow{line};
+        res.first[workflow.name] = workflow;
+        if (line.empty()) break;
+    }
+    for (std::string line; std::getline(in, line);) res.second.emplace_back(PartRatings{line});
+    return res;
+}
+
+int part1(const Input& input) {
+    auto& [workflows, ratings] = input;
+    return std::ranges::fold_left(
+        ratings //
+            | std::views::filter([&](auto& rating) {
+                  std::string s = "in";
+                  while (s != "A" && s != "R") {
+                      const auto it = workflows.find(s);
+                      if (it == end(workflows)) return false;
+                      s = it->second(rating);
+                  }
+                  return s == "A";
+              }) //
+            | std::views::transform([](auto& rating) { return rating.x + rating.m + rating.a + rating.s; }),
+        0, std::plus{});
+}
+
+struct RangeRatings {
+    struct Range {
+        int a{1};
+        int b{4000};
+        int len() const { return b - a + 1; }
+        bool valid() const { return a <= b; }
+    };
+    Range x;
+    Range m;
+    Range a;
+    Range s;
+    Range& get(std::string_view label) { return label == "x" ? x : label == "m" ? m : label == "a" ? a : s; }
+    int64_t value() const { return (int64_t)x.len() * m.len() * a.len() * s.len(); }
+};
+
+int64_t part2(const Input& input) {
+    int64_t res{};
+    auto& [workflows, ratings] = input;
+    std::stack<std::pair<std::string, RangeRatings>> st;
+    st.emplace("in", RangeRatings{});
+    while (!st.empty()) {
+        auto [wfname, currRating] = st.top();
+        st.pop();
+        if (wfname == "R") continue;
+        if (wfname == "A") {
+            res += currRating.value();
+            continue;
+        }
+        const auto it = workflows.find(wfname);
+        if (it == end(workflows)) continue;
+        bool valid = true;
+        for (auto& step : it->second.steps) {
+            auto& r = currRating.get(step.label);
+            auto nextRating = currRating;
+            auto& rn = nextRating.get(step.label);
+            if (step.lt) {
+                rn.b = std::min(rn.b, step.value - 1);
+                if (rn.valid()) st.emplace(step.next, nextRating);
+                r.a = std::max(r.a, step.value);
+            } else {
+                rn.a = std::max(rn.a, step.value + 1);
+                if (rn.valid()) st.emplace(step.next, nextRating);
+                r.b = std::min(r.b, step.value);
+            }
+            if (valid = r.valid(); !valid) break;
+        }
+        if (valid) st.emplace(it->second.last, currRating);
+    }
+    return res;
+}
+
+std::pair<bool, bool> test() {
+    auto testPart1 = [](std::istream& is, int correctAnswer) {
+        const auto input = parseInput(is);
+        const int answer = part1(input);
+        const bool correct = answer == correctAnswer;
+        fmt::print("Part 1: expected {}, got {}\n", correctAnswer,
+                   fmt::styled(answer, fmt::fg(correct ? fmt::color::green : fmt::color::red)));
+        return correct;
+    };
+    constexpr std::pair<std::string_view, int> part1Cases[] = {{R"(
+px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
+)",
+                                                                19114}};
+    bool part1Correct = true;
+    for (auto [sv, correctAnswer] : part1Cases) {
+        std::istringstream iss{sv.data()};
+        iss.ignore();
+        part1Correct &= testPart1(iss, correctAnswer);
+    }
+
+    auto testPart2 = [](std::istream& is, int64_t correctAnswer) {
+        const auto input = parseInput(is);
+        const auto answer = part2(input);
+        const bool correct = answer == correctAnswer;
+        fmt::print("Part 2: expected {}, got {}\n", correctAnswer,
+                   fmt::styled(answer, fmt::fg(correct ? fmt::color::green : fmt::color::red)));
+        return correct;
+    };
+    constexpr std::pair<std::string_view, int64_t> part2Cases[] = {{R"(
+px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
+)",
+                                                                    167409079868000}};
+    bool part2Correct = true;
+    for (auto [sv, correctAnswer] : part2Cases) {
+        std::istringstream iss{sv.data()};
+        iss.ignore();
+        part2Correct &= testPart2(iss, correctAnswer);
+    }
+    return {part1Correct, part2Correct};
+}
+
+int main() {
+    auto [test1, test2] = test();
+    if (!test1) return 1;
+    auto in = std::ifstream(kInputFilename.data());
+    if (!in) {
+        fmt::print("Cannot open '{}'\n", kInputFilename);
+        return -1;
+    }
+    const auto input = parseInput(in);
+    fmt::print("Part 1: {}\n", fmt::styled(part1(input), fmt::fg(fmt::color::yellow)));
+    if (!test2) return 2;
+    fmt::print("Part 2: {}\n", fmt::styled(part2(input), fmt::fg(fmt::color::yellow)));
+}
---
+++ day20/src/day20.cpp
@@ -0,0 +1,190 @@
+#include <fmt/format.h>
+#include <fmt/color.h>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <ranges>
+
+static constexpr std::string_view kInputFilename = "day20.txt";
+
+enum class Pulse { None, Low, High };
+struct Module {
+    virtual Pulse receivePulse(const std::string&, Pulse pulse) { return pulse; }
+    virtual void addInput(const std::string&) {}
+    virtual void reset() {}
+    virtual ~Module() {}
+};
+struct Broadcaster : Module {};
+struct FlipFlop : Module {
+    bool on = false;
+    Pulse receivePulse(const std::string&, Pulse pulse) override {
+        if (pulse == Pulse::None) return Pulse::None;
+        if (pulse == Pulse::High) return Pulse::None;
+        on = !on;
+        return on ? Pulse::High : Pulse::Low;
+    }
+    void reset() override { on = false; }
+};
+struct Conjunction : Module {
+    std::unordered_map<std::string, Pulse> memo;
+    void addInput(const std::string& inputName) override { memo[inputName] = Pulse::Low; }
+    Pulse receivePulse(const std::string& inputName, Pulse pulse) override {
+        memo[inputName] = pulse;
+        if (std::ranges::all_of(memo, [](auto& kv) { return kv.second == Pulse::High; })) return Pulse::Low;
+        return Pulse::High;
+    }
+    void reset() override {
+        for (auto& [k, v] : memo) v = Pulse::Low;
+    }
+};
+
+using Input = std::pair<std::unordered_map<std::string, std::unique_ptr<Module>>,
+                        std::unordered_map<std::string, std::vector<std::string>>>;
+
+Input parseInput(std::istream& in) {
+    std::vector<std::string> lines;
+    std::unordered_map<std::string, std::unique_ptr<Module>> modules;
+    for (std::string line; std::getline(in, line);) {
+        const std::string moduleRep = line.substr(0, line.find(' '));
+        if (moduleRep == "broadcaster") {
+            modules["broadcaster"] = std::make_unique<Broadcaster>();
+        } else {
+            const std::string moduleName = moduleRep.substr(1);
+            if (moduleRep[0] == '%') {
+                modules[moduleName] = std::make_unique<FlipFlop>();
+            } else {
+                modules[moduleName] = std::make_unique<Conjunction>();
+            }
+        }
+        lines.push_back(line);
+    }
+    std::unordered_map<std::string, std::vector<std::string>> adj;
+    for (auto& line : lines) {
+        const std::string fromRep = line.substr(0, line.find(' '));
+        const std::string from = fromRep[0] == 'b' ? fromRep : fromRep.substr(1);
+        std::vector<std::string> to;
+        std::istringstream iss{line.substr(line.find('>') + 1)};
+        for (std::string tok; iss >> tok;) {
+            if (tok.back() == ',') tok.pop_back();
+            to.push_back(tok);
+            if (auto it = modules.find(tok); it != end(modules)) it->second->addInput(from);
+        }
+        adj[from] = std::move(to);
+    }
+    return std::make_pair(std::move(modules), adj);
+}
+
+int part1(const Input& input) {
+    auto& [modules, adj] = input;
+    int hiCnt{};
+    int loCnt{};
+    for (int t = 0; t < 1000; ++t) {
+        std::queue<std::tuple<std::string, Pulse, std::string, int>> q;
+        q.emplace("broadcaster", Pulse::Low, "button", 0);
+        while (!q.empty()) {
+            auto [name, pulse, fromName, timestamp] = q.front();
+            q.pop();
+            if (pulse == Pulse::Low)
+                ++loCnt;
+            else if (pulse == Pulse::High)
+                ++hiCnt;
+            if (auto it = modules.find(name); it != end(modules)) {
+                const auto pulseOut = it->second->receivePulse(fromName, pulse);
+                if (pulseOut == Pulse::None) continue;
+                if (auto adjIt = adj.find(name); adjIt != end(adj))
+                    for (auto& nb : adjIt->second) q.emplace(nb, pulseOut, name, timestamp + 1);
+            }
+        }
+    }
+    return loCnt * hiCnt;
+}
+
+int64_t part2(const Input& input) {
+    auto& [modules, adj] = input;
+    for (auto& [k, v] : modules) v->reset();
+    std::string conjName =
+        std::ranges::find_if(adj, [](auto& kv) { return std::ranges::find(kv.second, "rx") != end(kv.second); })->first;
+    std::unordered_map<std::string, std::vector<int>> cycles;
+    if (auto* p = dynamic_cast<Conjunction*>(modules.find(conjName)->second.get()); p != nullptr)
+        for (auto k : p->memo | std::views::keys) cycles[k];
+    for (int t = 1; t < 1'000'000; ++t) {
+        if (std::ranges::all_of(cycles, [](auto& kv) -> bool { return kv.second.size() >= 2; })) break;
+        std::queue<std::tuple<std::string, Pulse, std::string, int>> q;
+        q.emplace("broadcaster", Pulse::Low, "button", 0);
+        while (!q.empty()) {
+            auto [name, pulse, fromName, timestamp] = q.front();
+            q.pop();
+            if (auto it = cycles.find(fromName); it != end(cycles))
+                if (pulse == Pulse::High) it->second.push_back(t);
+            if (auto it = modules.find(name); it != end(modules)) {
+                const auto pulseOut = it->second->receivePulse(fromName, pulse);
+                if (pulseOut == Pulse::None) continue;
+                if (auto adjIt = adj.find(name); adjIt != end(adj))
+                    for (auto& nb : adjIt->second) q.emplace(nb, pulseOut, name, timestamp + 1);
+            }
+        }
+    }
+    int64_t res{1};
+    for (auto& [k, v] : cycles) {
+        if (v[1] - v[0] != v[0]) {
+            fmt::print("Anomaly at {} {} {}\n", k, v[0], v[1]);
+            return -1;
+        }
+        res = std::lcm(res, v[1] - v[0]);
+    }
+    return res;
+}
+
+std::pair<bool, bool> test() {
+    auto testPart1 = [](std::istream& is, int64_t correctAnswer) {
+        const auto input = parseInput(is);
+        const auto answer = part1(input);
+        const bool correct = answer == correctAnswer;
+        fmt::print("Part 1: expected {}, got {}\n", correctAnswer,
+                   fmt::styled(answer, fmt::fg(correct ? fmt::color::green : fmt::color::red)));
+        return correct;
+    };
+    constexpr std::pair<std::string_view, int> part1Cases[] = {{R"(
+broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+)",
+                                                                32000000},
+                                                               {R"(
+broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
+)",
+                                                                11687500}};
+    bool part1Correct = true;
+    for (auto [sv, correctAnswer] : part1Cases) {
+        std::istringstream iss{sv.data()};
+        iss.ignore();
+        part1Correct &= testPart1(iss, correctAnswer);
+    }
+
+    bool part2Correct = true;
+    return {part1Correct, part2Correct};
+}
+
+int main() {
+    auto [test1, test2] = test();
+    if (!test1) return 1;
+    auto in = std::ifstream(kInputFilename.data());
+    if (!in) {
+        fmt::print("Cannot open '{}'\n", kInputFilename);
+        return -1;
+    }
+    const auto input = parseInput(in);
+    fmt::print("Part 1: {}\n", fmt::styled(part1(input), fmt::fg(fmt::color::yellow)));
+    if (!test2) return 2;
+    fmt::print("Part 2: {}\n", fmt::styled(part2(input), fmt::fg(fmt::color::yellow)));
+}
