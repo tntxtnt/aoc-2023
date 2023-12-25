5b2097dfa06b40859a800c526717a1a15b0eb107
Tri Tran <lepnit205@gmail.com>
2023-12-24 19:59:11 -0600

    Day 24 modernized

--- day24/src/day24.cpp
+++ day24/src/day24.cpp
@@ -120,24 +120,22 @@ bool witness(int64_t a, int64_t n, int64_t d, int64_t r) {
 }
 
 bool isPrimeDetMillerRabin(int64_t n) {
-    int64_t d, r; // n-1 = d * 2^r
-    for (d = n - 1, r = 0; d % 2 == 0; d >>= 1, r++) {
-    }
-    const int64_t arr[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
-    for (auto a : arr)
-        if (!witness(a, n, d, r)) return false;
-    return true;
+    int64_t d = n - 1;
+    int64_t r = 0;
+    for (; d % 2 == 0; d /= 2) ++r; // n-1 = d * 2^r
+    return ranges::all_of(std::array<int64_t, 7>{2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+                          [=](int64_t a) { return witness(a, n, d, r); });
 }
 
 int64_t pollardsRho(int64_t n) {
     auto g = [n](int64_t x) { return ((int128_t(x) * x + 1) % n).convert_to<int64_t>(); };
-    int64_t x = 2, y = 2, d = 1;
-    while (d == 1) {
+    int64_t d = 1;
+    for (int64_t x = 2, y = 2; d == 1;) {
         x = g(x);
         y = g(g(y));
         d = std::gcd(x > y ? x - y : y - x, n);
     }
-    // if (d == n) throw "Failture";
+    // if (d == n) throw "Failure";
     return d;
 }
 
@@ -146,11 +144,11 @@ std::unordered_map<int64_t, int> getPrimeFactors(int64_t n) {
     std::unordered_map<int64_t, int> res;
     if (n == 0) return res;
     // 2
-    while (n % 2 == 0) n /= 2, ++res[2];
-    // odds
-    const int iMax = 31623;
+    for (; n % 2 == 0; n /= 2) ++res[2];
+    // "small" odd integers
+    constexpr int iMax = 31623;
     for (int i = 3; i * i < n && i < iMax; i += 2)
-        while (n % i == 0) n /= i, ++res[i];
+        for (; n % i == 0; n /= i) ++res[i];
     while (n > 1) {
         // MR
         if (isPrimeDetMillerRabin(n)) {
@@ -166,36 +164,36 @@ std::unordered_map<int64_t, int> getPrimeFactors(int64_t n) {
     return res;
 }
 
-// https://stackoverflow.com/questions/5279051/how-can-i-create-cartesian-product-of-vector-of-vectors
-std::vector<std::vector<int64_t>> cart_product(const std::vector<std::vector<int64_t>>& v) {
-    std::vector<std::vector<int64_t>> s = {{}};
-    for (const auto& u : v) {
-        std::vector<std::vector<int64_t>> r;
-        for (const auto& x : s) {
-            for (const auto y : u) {
-                r.push_back(x);
-                r.back().push_back(y);
+template <class Fn>
+void forEachCartesianProduct(const std::vector<std::vector<int64_t>>& mat, Fn&& yield) {
+    std::vector<size_t> indices(mat.size());
+    std::vector<int64_t> out;
+    for (const auto& row : mat) out.push_back(row[0]);
+    const size_t n = ranges::fold_left(mat, size_t{1}, [](size_t m, const auto& row) { return m * row.size(); });
+    for (size_t i = 0; i < n; ++i) {
+        yield(out);
+        for (size_t j = indices.size(); j--;) {
+            if (++indices[j] < mat[j].size()) {
+                out[j] = mat[j][indices[j]];
+                break;
+            } else {
+                out[j] = mat[j][indices[j] = 0];
             }
         }
-        s = std::move(r);
     }
-    return s;
 }
 
 std::vector<int64_t> getDivisors(int64_t n) {
     const auto primeFactors = getPrimeFactors(n);
     std::vector<std::vector<int64_t>> powers;
     for (auto& [k, v] : primeFactors) {
-        std::vector<int64_t> p;
-        int64_t w{1};
-        for (int i = 0; i <= v; ++i) {
-            p.push_back(w);
-            if (i < v) w *= k;
-        }
+        std::vector<int64_t> p{1};
+        while (p.size() <= v) p.push_back(p.back() * k);
         powers.emplace_back(std::move(p));
     }
     std::vector<int64_t> res;
-    for (auto& g : cart_product(powers)) res.push_back(std::accumulate(begin(g), end(g), 1LL, std::multiplies{}));
+    forEachCartesianProduct(powers,
+                            [&](const auto& p) { res.push_back(ranges::fold_left(p, 1LL, std::multiplies{})); });
     ranges::sort(res);
     return res;
 }
@@ -235,19 +233,39 @@ int64_t part2(Input input) {
     const auto velY = getPossibleVels(input, [](auto& p) { return p.y; });
     const auto velZ = getPossibleVels(input, [](auto& p) { return p.z; });
     for (auto [vx, vy, vz] : views::cartesian_product(velX, velY, velZ)) {
-        std::vector<int64_t> times(input.size());
+        std::vector<int64_t> times;
         const auto& a = input[0];
-        if (!ranges::all_of(views::iota(size_t{1}, input.size()), [&](size_t i) {
-                const auto& b = input[i];
-                const auto p = integerIntersection(a.pos.x, a.pos.y, vx - a.vel.x, vy - a.vel.y, //
-                                                   b.pos.x, b.pos.y, vx - b.vel.x, vy - b.vel.y);
+        if (!ranges::all_of(input | views::drop(1), [&](const auto& b) {
+                /*                           B
+                                           /  /
+                                        /    /
+                A                    /     --
+                |\                v      /
+                |  \           %        /
+                 \   \        ^       --
+                  |    \     /      /
+                  |      v  /      /
+                  \        #     --
+                   |      ^    /
+                   |     /    /
+                   \    /   --
+                    |  /  /
+                    v / v
+                     R
+                ->   ->   ->
+                AR = A# + #R  --> vAR = vA - vR
+                ->   ->   ->
+                BR = B% + %R  --> vBR = vB - vR
+                */
+                const auto p = integerIntersection(a.pos.x, a.pos.y, a.vel.x - vx, a.vel.y - vy, //
+                                                   b.pos.x, b.pos.y, b.vel.x - vx, b.vel.y - vy);
                 if (!p) return false;
                 const auto t1 = static_cast<int64_t>(std::round(p->first));
                 const auto t2 = static_cast<int64_t>(std::round(p->second));
-                if (t1 >= 0 || t2 >= 0) return false;
-                if (times[0] == 0) times[0] = t1;
-                if (t1 != times[0]) return false;
-                times[i] = t2;
+                if (t1 <= 0 || t2 <= 0) return false;
+                if (times.empty()) times.push_back(t1);
+                if (t1 != times[0]) return false; // not the same R
+                times.push_back(t2);
                 return true;
             }))
             continue;
@@ -256,12 +274,12 @@ int64_t part2(Input input) {
                 return ranges::unique(v).begin() == end(v);
             }(times))
             continue;
-        const auto x = a.pos.x + (vx - a.vel.x) * times[0];
-        const auto y = a.pos.y + (vy - a.vel.y) * times[0];
-        const auto z = a.pos.z + (vz - a.vel.z) * times[0];
+        const auto x = a.pos.x + (a.vel.x - vx) * times[0];
+        const auto y = a.pos.y + (a.vel.y - vy) * times[0];
+        const auto z = a.pos.z + (a.vel.z - vz) * times[0];
         if (ranges::all_of(views::enumerate(times), [&](auto&& tp) {
                 auto&& [i, t] = tp;
-                return input[i].pos.z - t * input[i].vel.z == z - t * vz;
+                return input[i].pos.z + t * input[i].vel.z == z + t * vz;
             }))
             return x + y + z;
     }
